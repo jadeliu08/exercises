bcc965c3f3c9e98c34549612b74e48c3d2552293
jadeliu08 <changqingliu08@163.com>
2025-08-26 20:47:10 +0800

    init

--- promise.js
+++ promise.js
@@ -4,17 +4,30 @@ const STATUS = {
 	REJECTED: 'rejected'
 };
 
-class MyPromise {
+class Promise {
 	#status = STATUS.PENDING;
-	#result = undefined;
-	#thenables = [];
+	#value = undefined;
+	#reason = undefined;
+	#onCallbacks = [];
 
 	constructor(executor) {
 		const resolve = (value) => {
-			this.#changeStatus(STATUS.FULFILLED, value);
+			if (this.#status === STATUS.PENDING) {
+				this.#status = STATUS.FULFILLED;
+				this.#value = value;
+				this.#onCallbacks.forEach(({onResolved}) => {
+					onResolved(this.#value);
+				});
+			}
 		}
 		const reject = (reason) => {
-			this.#changeStatus(STATUS.REJECTED, reason);
+			if (this.#status === STATUS.PENDING) {
+				this.#status = STATUS.REJECTED;
+				this.#reason = reason;
+				this.#onCallbacks.forEach(({onRejected}) => {
+					onRejected(this.#reason);
+				});
+			}
 		}
 		try {
 			executor(resolve, reject);
@@ -23,36 +36,63 @@ class MyPromise {
 		}
 	}
 
-	#changeStatus(status, result) {
-		if (status !== STATUS.PENDING) {
-			return;
-		}
-		this.#status = status;
-		this.#result = result;
-		this.#run();
-	}
-
-	#run() {
-		if (this.#status === STATUS.PENDING) {
-			return;
-		}
-	}
-
-	then(onFullFilled, onRejected) {
-		return new MyPromise((resolve, reject) => {
-			this.#thenables.push({
-				onFullFilled,
-				onRejected,
-				resolve,
-				reject,
-			});
-			// 这里为啥要执行run呢??
-			this.#run();
+	then(onResolved, onRejected) {
+		const that = this;
+		const promise2 = new Promise((resolve, reject) => {
+			if (that.#status === FULFILLED) {
+				const x = onResolved(this.#value);
+				resolvePromise(promise2, x, resolve, reject);
+			}
+			if (that.#status === REJECTED) {
+				const x = onRejected(this.#reason);
+				resolvePromise(promise2, x, resolve, reject);
+			}
+			if (that.#status === PENDING) {
+				this.#onCallbacks.push({
+					onResolved: (value) => {
+						const x = onResolved(value);
+						resolvePromise(promise2, x, resolve, reject);
+					},
+					onRejected: (reason) => {
+						const x = onRejected(reason);
+						resolvePromise(promise2, x, resolve, reject);
+					}
+				});
+			}
 		});
+		return promise2;
 	}
 }
 
-const p = new MyPromise((resolve, reject) => {
-	throw 111;
-});
-console.log('p', p);
+Promise.resolve = function (value) {
+	return new Promise((resolve) => {
+		resolve(value);
+	});
+}
+
+Promise.reject = function (reason) {
+	return new Promise((resolve, reject) => {
+		reject(reason);
+	});
+}
+
+Promise.race = function (promises) {
+	return new Promise((resolve, reject) => {
+		for (var i = 0; i < promises.length; i++) {
+			promises[i].then(resolve, reject);
+		}
+	});
+}
+
+Promise.all = function (promises) {
+
+}
+
+function resolvePromise(promise2, x, resolve, reject) {
+	if (x === promise2) {
+		return reject(new TypeError('Chaining cycle detached for promise'));
+	}
+	if (x !== null && ['object', 'function'].includes(typeof x)) {
+
+	}
+}
